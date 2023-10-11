e2ae7e701425d22acbe90f648222dfc1a5d52fd6
elkalt <elliott.kalt@gmail.com>
2023-10-11 10:33:37 -0400

    changed about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,7 +9,7 @@
 
 <div class="content-wrapper">
   <h1>About</h1>
-  <p>Built using SvelteKit.  See the code on <a href="https://github.com/elkalt/ccubed">GitHub</a>.</p>
+  <p>Built in 4 hours using SvelteKit.  See the code on <a href="https://github.com/elkalt/ccubed">GitHub</a>.</p>
   <p>Cities were collected from the Geonames dump and filtered to only include those with more than 50 000 population.</p>
 </div>
 
